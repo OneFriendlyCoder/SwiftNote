1061afa5e8cf4e2255ba1bf27c758dfc38978e94
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-13 16:31:06 +0530

    signup logic done

--- src/Signup/SignUp.tsx
+++ src/Signup/SignUp.tsx
@@ -15,7 +15,6 @@ const SignUp = () => {
 
  	const REGISTER_URL = "/signup";
 
-	const Register = () => {
 		const userRef = useRef<HTMLInputElement>(null);				// this is not valid change this
 		const errRef = useRef();
 
@@ -51,9 +50,31 @@ const SignUp = () => {
 		useEffect(()=>{                            // this will be called whenever the email changes
 			const result = EMAIL_REGEX.test(email);
 			setValidEmail(result);
-		},[email])
-
+		},[email])	
 		
+	const handleSubmit = async (e:React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		console.log("submit is enabled")
+		console.log({user, email, pwd})
+		const v1 = USER_REGEX.test(user);
+		const v2 = PWD_REGEX.test(pwd);
+		if(!v1 || !v2){
+			setErrMsg("Invalid Entry");
+			return;
+		}
+		try {
+			const response = await axios.post(REGISTER_URL, JSON.stringify({user, pwd, email}), {headers: {'Content-Type': 'application/json'}, withCredentials: true});
+			console.log(JSON.stringify(response))
+			setSuccess(true);
+			setUser("");
+			setPwd("");
+			setEmail("");
+		} catch (error) {
+			const err = error as AxiosError;
+			if(err?.response)setErrMsg("No server response");
+			else if(err.response?.status===409) setErrMsg("Username taken");	// this could be specified when writing the backend server
+			else {setErrMsg("Registration failed")}
+		}
 	}
 
   return (
@@ -64,12 +85,14 @@ const SignUp = () => {
 	<div className="form-structor">
 		<div className="signup">
 			<h2 className="form-title" id="signup">Sign up</h2>
+			<form>
 			<div className="form-holder">
-				<input type="text" className="input" placeholder="Name" />
-				<input type="email" className="input" placeholder="Email" />
-				<input type="password" className="input" placeholder="Password" />
+				<input type="text" className="input" placeholder="Name" onChange={(e)=>{setUser(e.target.value)}} ref={userRef} autoComplete='off' onFocus={()=>{setUserFocus(true)}} onBlur={()=>{setUserFocus(false)}}/>
+				<input type="email" className="input" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} autoComplete='off' onFocus={()=>{setEmailFocus(true)}} onBlur={()=>{setEmailFocus(false)}}/>
+				<input type="password" className="input" placeholder="Password" onChange={(e)=>{setPwd(e.target.value)}} autoComplete='off' onFocus={()=>{setPwdFocus(true)}} onBlur={()=>{setPwdFocus(false)}}/>
 			</div>
-			<button className="submit-btn">Sign up</button>
+			</form>
+			<button className="submit-btn" disabled={!validName || !validEmail || !validPwd ? true: false} onClick={handleSubmit}>Sign up</button>
 			<button className="submit-btn" onClick={()=>{navigate("/login")}}>Login</button>
 		</div>
 	</div>
