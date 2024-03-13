5d941b2258e290808edc460d9270a3c521bb3a5a
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-13 13:19:51 +0530

    login and signup page styling 2.0

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -4,7 +4,10 @@ import dino1 from '../assets/dino.mp4'
 
 const Login = () => {
   return (
-	<div className='pt1'>
+	<div className='pt2'>
+		<video autoPlay muted loop id="background-video2">
+        	<source src={dino1} type="video/mp4" />
+      	</video>
 	<div className="form-structor1">
 		<div className="login">
 			<h2 className="form-title1" id="login">Login</h2>
--- src/Signup/SignUp.tsx
+++ src/Signup/SignUp.tsx
@@ -5,6 +5,9 @@ import dino1 from '../assets/dino.mp4'
 const SignUp = () => {
   return (
 	<div className='pt1'>
+	    <video autoPlay muted loop id="background-video1">
+        	<source src={dino1} type="video/mp4" />
+      	</video>
 	<div className="form-structor">
 		<div className="signup">
 			<h2 className="form-title" id="signup">Sign up</h2>
@@ -17,7 +20,7 @@ const SignUp = () => {
 		</div>
 	</div>
 	</div>
-  )
+)
 }
 
 export default SignUp
